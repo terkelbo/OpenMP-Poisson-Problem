edb65bd125ed5f9d10a28ce9f2b7d1907ac231db
terkelbo <terkel_olsen@hotmail.com>
2019-01-17 10:58:46 +0100

    changed statfun to work for all parallelization techniques

--- statfun.sh
+++ statfun.sh
@@ -15,21 +15,23 @@ module load studio
 
 
 THREADS="1 2 4 8 16"
-N="10 20 30 50 100 150 200 250 500 1000 1250 1500 2000 2800"
+N="10 20 30 50 100 150 200 250 500 1000 1250 1500 2000 2500 3000"
+IMPLEMENTATIONS="poisson_naive poisson_openmp1 poisson_openmp2"
 
-rm -f data/statfun.dat
-
-echo "Making data file containing stats - see data/statfun.dat"
-for t in $THREADS
-	do 
-	for rows in $N
-		do
-		echo "OMP_WAIT_POLICY=active OMP_NUM_THREADS=$t OMP_SCHEDULE=dynamic,8 ./poisson_openmp1 $rows"
-		output=$(OMP_WAIT_POLICY=active OMP_NUM_THREADS=$t OMP_SCHEDULE=dynamic,8 ./poisson_openmp1 $rows)
-		echo "$t $output $rows" >> data/statfun.dat
+for impl in $IMPLEMENTATIONS
+do
+	rm -f data/statfun_$impl.dat
+	echo "Making data file containing stats - see data/statfun_$impl.dat"
+	for t in $THREADS
+		do 
+		for rows in $N
+			do
+			echo "OMP_WAIT_POLICY=active OMP_NUM_THREADS=$t OMP_SCHEDULE=dynamic,8 ./$impl $rows"
+			output=$(OMP_WAIT_POLICY=active OMP_NUM_THREADS=$t OMP_SCHEDULE=dynamic,8 ./$impl $rows)
+			echo "$t $output $rows" >> data/statfun_$impl.dat
+		done
 	done
 done
-
 echo "File is done"
 
 exit 0
