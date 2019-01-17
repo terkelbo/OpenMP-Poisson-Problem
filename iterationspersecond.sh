1afd85c14a34c18b2ff9d641099143e36f31ccae
terkelbo <terkel_olsen@hotmail.com>
2019-01-17 21:58:23 +0100

    Add iterations/second and simple convergence plots

---
+++ iterationspersecond.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ALGO="jacobi gauss"
+N="10 20 30 50 100 150 200 250 500 1000 1250 1500 2000 2500 2830"
+
+rm -f iterationpersecond.dat
+for algo in $ALGO
+do
+	for rows in $N
+	do
+		output=$(./poisson_openmp1 $rows $algo)
+		echo "$algo $output" >> iterationspersecond.dat
+	done
+done
---
+++ vizconvergence.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+from matplotlib.ticker import ScalarFormatter
+
+df1 = pd.read_csv('convergence.dat',delim_whitespace=True,header=None,names=["Algorithm", "Iterations", "Euclidian Norm"])
+
+plt.figure()
+df1.set_index("Iterations", inplace=True)
+ax = df1.groupby("Algorithm")["Euclidian Norm"].plot(legend=True)
+plt.legend(loc='upper right')
+plt.xlabel('Nb. of iterations')
+plt.ylabel('Euclidian Norm')
+plt.savefig('convergence.png', bbox_inches='tight')
+plt.close()
+
+df1 = pd.read_csv('iterationspersecond.dat',delim_whitespace=True,header=None,names=["Algorithm", "Iterations", "Memory", "MFlops", "Wall Time"])
+df1['iterationspersecond'] = df1['Wall Time'] / df1["Iterations"]
+plt.figure()
+df1.set_index("Memory", inplace=True)
+ax = df1.groupby("Algorithm")["iterationspersecond"].plot(legend=True)
+plt.legend(loc='upper right')
+plt.xlabel('Memory (Kbytes)')
+plt.ylabel('Iterations per second')
+plt.xscale('log',basex=4)
+ax = plt.gca().xaxis 
+ax.set_major_formatter(ScalarFormatter()) 
+plt.savefig('iterationspersecond.png', bbox_inches='tight')
+plt.close()
+
